44d1e3aa3622b62b6c35615b08c8dd8112137562
rengatesakshi9-source <rengatesakshi9@gmail.com>
2026-03-27 09:12:56 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,110 @@
+#print(val.index(1))
+#print(val.index(2))
+#print(val.index(3))
+#print(val.index(4))
+#print(val.index(5))
+#print(val.index(6))
+#print(val.index(7))
+# x=['A','B','C']
+# y=['A','B','C']
+# z=[1,2,3,4]
+# print(x==y)
+# print(x==z)
+# print(x !=z)
+# import datetime
+#datetime formating
+# date=datetime.datetime.now()
+# print("It's now:{:%d/%m/%y %H:%M:%S}".format(date))
+# for i in range(1,5):
+#     if i==3:
+#         continue
+#     print(i)
+# mycart=[10,20,30,200,400,50,800,900]
+# for i in mycart:
+#     if i>400:
+#         print("this my purchased cart item")
+#         continue
+#     print(i)
+# list=[1,2,3,4,5,6,7,8,9,10]
+# sum=0
+# for x in list:#x=0:1
+#     sum=sum+x
+# print("The Sum=",sum)
+# rollno=[3,5,7,1,11,4,5,2]
+# for x in rollno:
+#     if x==2 or x==4 or x==6 or x==8 or x==10:
+#         print("even no is found",x)
+#         break
+# name="sakshi"
+# newname=" "
+# for i in name:
+#     if i not in newname:
+#         newname += i
+# print(newname)
+# name = "prashant"
+# i=0
+# for x in name:
+#     if x == 'n':
+#         print("The character present at index no ",i,"value=:",x)
+#         break
+#     i=i+1
+
+# name="sakshi"
+# vow=0
+# cons=0
+# vowels =['a','e','i','o','u']
+# for i in name:
+#     if i in vowels:
+#         vow += 1
+#     else:
+#         cons +=1
+# print(" vowels=",vow)
+# print("consonent=",cons)
+
+# for i in range(5,0,-1):
+#     print(i)
+# fact=1
+# for i in range(5,0,-1):
+#     fact =fact*i
+# print(fact)
+# mydict={
+# name"":"sandhya ",
+# "prefessional":"developer",
+# "empid":1001
+# }
+# print(mydict)
+# mydict={
+# 101:"sakshi",
+# 102:"sandhya",
+# "103":"mohini",
+# "104":"madhura",
+# 101:"sandhya",
+# 143:"ningu"
+# }
+# print(mydict)
+
+# a=mydict[101]
+# print(a)
+# mydict[102]="shiva"
+# print(mydict)
+#only print key x=0,1
+# for x in mydict:
+#     print(x)
+# only print values
+# for x in mydict.values():
+#     print(x)
+
+# only print key and values
+# for x ,y in mydict.items():
+#     print(x,y)
+# mydict["mobile no"]=9535589510
+# print(mydict)
+# mydict.pop(101)
+# print(mydict)
+# mydict={
+#  "name":"sandhya ",
+#  "prefessional":"developer",
+#  "empid":1001
+# }
+# newdict=mydict.copy()
+# print(newdict)
