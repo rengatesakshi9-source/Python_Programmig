1a5b006d12271d525565abfbccd2b1f3a473727a
rengatesakshi9-source <rengatesakshi9@gmail.com>
2026-03-27 09:11:57 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,55 @@
+#mylist=['sakshi','sandhya','madhu','diksha','jyo']
+#print(mylist)
+#mylist.insert(1,'sanket')
+#print(mylist)#using insert adding new value
+#mylist.remove("diksha")
+#print(mylist)
+#mlist=[['sakshi','Rengate'],[89.32],[9777777,"yyyyy"]]
+#print("example of multidimensional list:")
+#print(mlist)
+#print(mlist[0][0])
+#print(mlist[0][1])
+#print(mlist[1][0])
+#print(mlist[2][0])
+#list1=["prashant","jha"]
+#print(list1*2)
+#list2=[50,25.50]
+#print(list1+list2)
+#list2 =[50,25.50,'sandhya']
+#del list2[2]
+#del list2
+#print(list2)
+#v=[1,2,3,4,5]
+#for i in v:
+  #  print(i)
+#list3=[50,25.50,'sandhya']
+#list3.clear()
+#print(list3)
+#name="sakshi"
+#print(name)
+#myname=list(name)#typecasting=converting str into list
+#print(myname)
+#mylist=[50,30,10,20]
+#mylist.reverse()
+#print(mylist)
+#mylist=[44,24,77,89,81,90]
+#mylist.sort(reverse=true)
+#print(myl
+#mylist=[44,24,77,89,81,90]
+#newlist = mylist
+#print(id(mylist))
+#print(id(newlist))
+#mytuple=("sakshi","sandhya","madhu","diksha","jyo")
+#print(mytuble)
+#print(type(mytuple))
+#print('z' in "sakshi")
+#print('z' not in "sakshi")
+#print('a' in "sakshi")
+#mylist = [3,5,7,9,2]
+#print(3 in mylist)
+#a = 10
+#b = 15
+#print(id(a))
+#print(id(b))
+#print(a is b)
+#print(a is not b)
