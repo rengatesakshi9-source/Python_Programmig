222fffd993676c6f902a3c6a2c9c07ef91c72ef0
rengatesakshi9-source <rengatesakshi9@gmail.com>
2026-03-27 09:13:29 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,17 @@
+#a=int(input("Enter first number:"))
+#b=int(input("Enter second number:"))
+#c=int(input("Enter three number:"))
+#if n1>n2 and n1>n3:
+     #print("Biggest number is:",a)
+#elif n2>n3:
+    #print("Biggest number is:",b)
+#else:
+  #  print("Biggest number is:",c)
+count=0
+for i in  range(9):
+      if i%2 ==0:
+          print(i)
+      else:
+          print(i)
+          count +=1
+print("count =",count)
