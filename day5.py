ddaa6678b9c8e97168b059157e444065db6414eb
rengatesakshi9-source <rengatesakshi9@gmail.com>
2026-04-17 18:24:23 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,65 @@
+# mylist=[0,1,0,3,12]
+# mylist.sort(reverse=True)
+# print(mylist)
+# list=[0,1,0,3,12]
+# for i in list:
+#     if i==0:
+#         list.remove(i)
+#         list.append(i)
+# print(list)
+
+# print the common number
+# a=[1,2,3]
+# b=[2,3,4]
+# c=[3,4,5]
+# for i in a:
+#     if i in b and i in c:
+#         print(i)
+#while loop syntax
+# even=0
+# odd=0
+# i=1
+# while i<=10:
+#     if i%2==0:
+#         even=even+1
+#     else:
+#         odd=odd+1
+#     i=i+1
+#     print("even=",even)
+#     print("odd=")
+# sum=0
+# i=1
+# while i<=10:
+#     sum=sum+i
+#     i=i+1
+#     print(sum)
+# mul=1
+# i=1
+# while i<=5:
+#     mul=mul*i
+#     print(mul)
+#     i=i+1
+# for i,j in zip(range(1,6),range(5,0,-1)):
+#     if i==3 and j==3:
+#         continue
+#     print(i," ",j)
+#nested for loop
+# n=int(input("enter the number of rows:"))
+# for i in range (1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end=" ")
+#     print()
+# n=int(input("enter the number of rows:"))
+# for i in range (1,n+1):
+#     for j in range(1,1+i):
+#         print (i,end=" ")
+#     print()
+
+
+
+
+
+
+
+
+
